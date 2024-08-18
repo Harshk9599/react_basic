2f7f31ae1d6fdaedf87edc9c64f1a1eb56f01bfe
Harshk9599 <harshkashyap14321@gmail.com>
2024-08-18 21:10:11 +0530

    commit2@18-8-2024

---
+++ 01basic_react/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <>
+    <h1>Chai or React</h1>
+    <h1>Chai or React</h1>
+    <h1>Chai or React</h1>
+    <h1>Chai or React</h1>
+    <h1>Chai or React</h1>
+    <h1>Chai or React</h1>
+    </>
+  );
+}
+
+export default App;
